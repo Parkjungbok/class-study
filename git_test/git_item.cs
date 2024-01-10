9fc29a90e55df87400666d380288b1ac3fc52a50
Parkjungbok <sumeing.vogi@gmail.com>
2024-01-11 00:34:03 +0900

    feat : 아이템기능
    테스트2

---
+++ git_test/git_item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_test
+{
+    internal class git_item
+    {
+        //아이템정보
+    }
+}
